b16224e98ddcdec81905c06f50a51fa9bc87becc
Kamal Garg <kamal.garg@delhivery.com>
2017-03-28 16:33:58 +0530

    Change in master

--- learngit/src/main/java/com/learn/StartApplication.java
+++ learngit/src/main/java/com/learn/StartApplication.java
@@ -3,6 +3,7 @@ package com.learn;
 public class StartApplication {
 
     public static void main(String[] args){
+        System.out.println("Change in master");
         System.out.println("Hello");
     }
 }
